6a2d3e40a0df0d6414598943c3336c6dcfda6601
moreta <moretajoo@gmail.com>
2016-08-27 23:19:37 +0900

    change dist asset path for gh-pages

--- examples/config/index.js
+++ examples/config/index.js
@@ -6,7 +6,7 @@ module.exports = {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
+    assetsSubDirectory: 'vue-search-select/static',
     assetsPublicPath: '/',
     productionSourceMap: true
   },
