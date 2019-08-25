4cbe2e0a15783f993cc388b64a219d2c86f333d2
moreta <moretajoo@gmail.com>
2019-08-26 01:50:52 +0900

    lib css directory change

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,4 @@
-import '../assets/app.scss'
+import '../assets/lib/lib.scss'
 import BasicSelect from './BasicSelect.vue'
 import ListSelect from './ListSelect.vue'
 import MultiListSelect from './MultiListSelect.vue'
